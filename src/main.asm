0f55b17be6d49616f353580db0caaee9c904b891
Reza-namvaran <reza.namvaran823@gmail.com>
2025-09-07 23:29:41 +0330

    (feat): Implement movable drawing brush

--- src/main.asm
+++ src/main.asm
@@ -4,22 +4,67 @@ org 0x100
 %include "include/macros.inc"
 
 start:
-    ; set VGA 13h
+    ; Set VGA 13h
     mov ax, 0x0013
     int 0x10
 
-    ; point ES to video memory
+    ; Point ES to video memory
     mov ax, VIDEO_SEGMENT
     mov es, ax
 
-    ; test pixel: (100,50), red
-    PLOT_PIXEL 100, 50, 12
+    ; Set initial cursor position (center)
+    mov cx, SCREEN_WIDTH/2
+    mov dx, SCREEN_HEIGHT/2
 
-    ; wait for key
+main_loop:
+    ; Wait for key press
     mov ah, 0
-    int 0x16
+    int 0x16            ; AL = ASCII, AH = scan code
 
-    ; restore text mode
+
+    cmp ah, KEY_UP
+    je move_up
+    cmp ah, KEY_DOWN
+    je move_down
+    cmp ah, KEY_LEFT
+    je move_left
+    cmp ah, KEY_RIGHT
+    je move_right
+    cmp ah, KEY_ESC
+    je quit
+
+    jmp main_loop
+
+move_up:
+    cmp dx, 0
+    jz main_loop
+    dec dx
+    jmp draw_cursor
+
+move_down:
+    cmp dx, SCREEN_HEIGHT-1
+    jz main_loop
+    inc dx
+    jmp draw_cursor
+
+move_left:
+    cmp cx, 0
+    jz main_loop
+    dec cx
+    jmp draw_cursor
+
+move_right:
+    cmp cx, SCREEN_WIDTH-1
+    jz main_loop
+    inc cx
+    jmp draw_cursor
+
+draw_cursor:
+    PLOT_PIXEL cx, dx, COLOR_WHITE
+    jmp main_loop
+
+quit:
+    ; Restore text mode
     mov ax, 0x0003
     int 0x10
     ret
